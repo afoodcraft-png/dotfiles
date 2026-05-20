8b495dc9447a456586259eb33b0b3b4c026f2035
afoodcraft-png <afoodcraft@gmail.com>
2026-05-20 23:54:47 +0700

    fix: handle conflicts in install script

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 echo "==> Installing packages..."
 sudo pacman -S --needed git stow i3 polybar rofi alacritty picom nitrogen dunst
 
+echo "==> Removing existing configs..."
+rm -rf ~/.config/i3
+rm -rf ~/.config/polybar
+rm -rf ~/.config/rofi
+rm -rf ~/.config/alacritty
+rm -rf ~/.config/picom
+rm -f ~/.zshrc
+
 echo "==> Stowing dotfiles..."
 cd ~/dotfiles
 
@@ -14,3 +22,5 @@ stow picom
 stow zsh
 
 echo "==> Done! Please restart your session."
+
+
